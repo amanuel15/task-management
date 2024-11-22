d9751e3463d6539b30524bad1f0a8422819b86cf
amanuel15 <amanuelgenene11@gmail.com>
2024-11-22 22:34:04 +0300

    feat(backend): create prisma model and validation middleware

---
+++ backend/src/middlewares/validateMiddleware.ts
@@ -0,0 +1,28 @@
+import { Schema } from 'zod';
+import { Request, Response, NextFunction } from 'express';
+
+export default function validate(
+  schema: Schema,
+  field: 'body' | 'query' = 'body'
+) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const val = schema.safeParse(field === 'query' ? req.query : req.body);
+    if (val.success) {
+      if (field === 'body') {
+        req.body = val.data;
+      }
+      next();
+    } else {
+      let errorMessage = '';
+      const json = JSON.parse(val.error.message);
+      if (Array.isArray(json) && json.length) {
+        errorMessage = `${json[0].message} for '${
+          json[0].path[1] || json[0].path[0]
+        }'${json[0].path[1] ? ` at index ${json[0].path[0]}.` : '.'}`;
+      }
+      res.status(400).json({
+        msg: errorMessage
+      });
+    }
+  };
+}
