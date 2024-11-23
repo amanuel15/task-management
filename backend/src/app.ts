1c929899fb8d6b0d0404b16ea594270ddfbc6cbb
amanuel15 <amanuelgenene11@gmail.com>
2024-11-23 12:46:34 +0300

    feat(backend): configure testing with ts-jest and supertest

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,9 @@ import { authRoutes, taskRoutes } from '@/routes';
 const app = express();
 
 app.use(express.json());
+app.use('/healthcheck', (_, res) => {
+  res.status(200).json({ status: 'ok' });
+});
 app.use('/auth', authRoutes);
 app.use('/api/task', taskRoutes);
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,5 +4,5 @@ import app from '@/app';
 import { PORT } from '@/constants';
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on "http://localhost:${PORT}"`);
 });
