fb7fe49db128e8827ccf441c0c4e58bf2570fd13
amanuel15 <amanuelgenene11@gmail.com>
2024-11-23 18:19:31 +0300

    feat(backend): create task services

---
+++ backend/src/services/taskService.ts
@@ -0,0 +1,26 @@
+import prisma from '@/models/prismaClient';
+import { taskSchemas } from '@/schemas';
+
+export function findMyTasks(userId: string) {
+  return prisma.task.findMany({ where: { userId } });
+}
+
+export function findMyTask(taskId: string, userId: string) {
+  return prisma.task.findUnique({ where: { id: taskId, userId } });
+}
+
+export function createTask(task: taskSchemas.CreateTaskSchema, userId: string) {
+  return prisma.task.create({ data: { ...task, userId } });
+}
+
+export function updateTask(
+  taskId: string,
+  task: taskSchemas.UpdateTaskSchema,
+  userId: string
+) {
+  return prisma.task.update({ where: { id: taskId, userId }, data: task });
+}
+
+export function deleteTask(taskId: string, userId: string) {
+  return prisma.task.delete({ where: { id: taskId, userId } });
+}
